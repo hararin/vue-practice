c02a7b2eddd7078d2fcd9378675e941058b17fdd
hararin <69122070+hararin@users.noreply.github.com>
2021-09-29 19:05:50 +0900

    first commit

---
+++ app/javascript/EmployeeFormPane.vue
@@ -0,0 +1,51 @@
+<template>
+  <form @submit.prevent="$emit('submit')">
+    <div v-if="errors.length != 0">
+      <ul v-for="e in errors" :key="e">
+        <li><font color="red">{{ e }}</font></li>
+      </ul>
+    </div>
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="employee.name">
+    </div>
+    <div>
+      <label>Department</label>
+      <input type="text" v-model="employee.department">
+    </div>
+    <div>
+      <label>Gender</label>
+      <select v-model="employee.gender">
+        <option>other</option>
+        <option>male</option>
+        <option>female</option>
+      </select>
+    </div>
+    <div>
+      <label>Birth</label>
+      <input type="date" v-model="employee.birth">
+    </div>
+    <div>
+      <label>Joined Date</label>
+      <input type="date" v-model="employee.joined_date">
+    </div>
+    <div>
+      <label>Payment</label>
+      <input type="number" v-model="employee.payment" min="0">
+    </div>
+    <div>
+      <label>Note</label>
+      <input v-model="employee.note" type="text">
+    </div>
+    <button type="submit">Commit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    employee: {},
+    errors: ''
+  }
+}
+</script>
---
+++ app/javascript/app.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import VueRouter from 'vue-router'
+
+  import EmployeeIndexPage from 'EmployeeIndexPage.vue'
+  import EmployeeDetailPage from 'EmployeeDetailPage.vue'
+  import EmployeeNewPage from 'EmployeeNewPage.vue'
+  import EmployeeEditPage from 'EmployeeEditPage.vue'
+
+  const router = new VueRouter({
+    routes: [
+      {
+        path: '/',
+        name: 'EmployeeIndexPage',
+        component: EmployeeIndexPage
+      },
+      {
+        path: '/employees/:id(\\d+)',
+        name: 'EmployeeDetailPage',
+        component: EmployeeDetailPage
+      },
+      {
+        path: '/employees/new',
+        name: 'EmployeeNewPage',
+        component: EmployeeNewPage
+      },
+      {
+        path: '/employees/:id(\\d+)/edit',
+        name: 'EmployeeEditPage',
+        component: EmployeeEditPage
+      }
+    ]
+  })
+
+  Vue.use(VueRouter)
+
+  export default {
+    router
+  }
+</script>
